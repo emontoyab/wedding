c67db6a4a381d72dd318ce9b10a143e562c79771
Esteban Montoya <emontoyab93@gmail.com>
2025-02-12 00:28:42 -0500

    Add final details

--- wedding.py
+++ wedding.py
@@ -21,59 +21,50 @@ image_map = {
     "imagecm" : "pics/Conrado.jpg",
     "imagedc" : "pics/DanielaCardona.jpg",
     "imagefc" : "pics/FamiliaCastro.jpg",
+    "imagejg" : "pics/juandagallo.jpg",
     "imagejl" : "pics/Juanes.jpg",
+    "imagejj" : "pics/juanjohenao.jpg",
     "imagekg" : "pics/KatheGallego.jpg",
     "imagelz" : "pics/LauraZapata.jpg",
     "imagelr" : "pics/Leidy.jpg",
-    "imagelg" : "pics/LuisaJuanGallo.jpg",
+    "imagelm" : "pics/loremakeup.jpg",
+    "imagelg" : "pics/luisagallina.jpg",
     "imagepj" : "pics/PaolaJuan.jpg",
     "imagemt" : "pics/Martín.jpg",
     "imageyb" : "pics/Yessica.jpg",
+    "imageyr" : "pics/yuliethrobin.jpg",
 }
 
-if "imageaoa" in query_params:
-    image_path = image_map["imageaoa"]
-elif "imagear" in query_params:
-    image_path = image_map["imagear"]
-elif "imagealx" in query_params:
-    image_path = image_map["imagealx"]
-elif "imageag" in query_params:
-    image_path = image_map["imageag"]
-elif "imagecg" in query_params:
-    image_path = image_map["imagecg"]
-elif "imageca" in query_params:
-    image_path = image_map["imageca"]
-elif "imagecm" in query_params:
-    image_path = image_map["imagecm"]
-elif "imagedc" in query_params:
-    image_path = image_map["imagedc"]
-elif "imagefc" in query_params:
-    image_path = image_map["imagefc"]
-elif "imagejl" in query_params:
-    image_path = image_map["imagejl"]
-elif "imagekg" in query_params:
-    image_path = image_map["imagekg"]
-elif "imagelz" in query_params:
-    image_path = image_map["imagelz"]
-elif "imagelr" in query_params:
-    image_path = image_map["imagelr"]
-elif "imagelg" in query_params:
-    image_path = image_map["imagelg"]
-elif "imagepj" in query_params:
-    image_path = image_map["imagepj"]
-elif "imagemt" in query_params:
-    image_path = image_map["imagemt"]
-elif "imageyb" in query_params:
-    image_path = image_map["imageyb"]
+image_path = next((image_map.get(key) for key in query_params if key in image_map), None)
 
-# st.video('pics/invitacion 20%.mp4', loop = True, autoplay=True)
-st.image("pics/invitacion 20%.gif")
-st.image(image_path)
-with st.expander("Ubicación"):
-    st.title("Ubicación")
-    location_map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.035675216696!2d-75.41892872627282!3d6.1259016276586316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e469fbde4169acb%3A0x704574a6c9fbb8b5!2sLa%20Jacinta%20Casa%20Boutique!5e0!3m2!1ses!2sco!4v1739253697287!5m2!1ses!2sco" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
-    st.markdown(location_map, unsafe_allow_html=True)
-with st.expander("Código de Vestimenta"):
-    st.text("Tablero de Pinterest")
-with st.expander("Confirmar asistencia"):
-    st.text("Confirmar Asistencia")
+if image_path is not None:
+    # st.video('pics/invitacion 20%.mp4', loop = True, autoplay=True)
+    st.image("pics/invitacion 20%.gif")
+    st.image(image_path)
+    with st.expander("Ubicación"):
+        # st.text("Ubicación")
+        location_map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.035675216696!2d-75.41892872627282!3d6.1259016276586316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e469fbde4169acb%3A0x704574a6c9fbb8b5!2sLa%20Jacinta%20Casa%20Boutique!5e0!3m2!1ses!2sco!4v1739253697287!5m2!1ses!2sco" width="100%" height="200px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+        st.markdown(location_map, unsafe_allow_html=True)
+        
+    with st.expander("Código de Vestimenta"):
+        # st.text("A continuación, encontrarás referencias de qué vestuario utilizar en la boda.")
+
+        # URL del tablero de Pinterest (cambia esto con tu tablero)
+        pinterest_url = "https://www.pinterest.com/zuliramcs/wedding-clothes/"
+
+        image_url = "pics/pinterest.png"
+
+        # HTML para hacer que la imagen sea un enlace clickeable
+        # st.html("<img src='pics/pinterest.png'>")
+
+        st.image("pics/pinterest.png", use_container_width=True)
+        st.link_button("Ver más", url=pinterest_url)
+
+        # # Botón para abrir el tablero
+        # st.markdown(f"[👉 Ver el tablero en Pinterest]({pinterest_url})")
+
+        
+    # with st.expander("Confirmar asistencia"):
+    #     st.text("Confirmar Asistencia")
+else:
+    st.title("Hay un error con tu link, por favor contacta a Esteban")
